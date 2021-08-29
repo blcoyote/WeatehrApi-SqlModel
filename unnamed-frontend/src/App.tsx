c200ff4ad4052203d8fe5c70ed55b0b4313fef37
blc <blc@elcoyote.dk>
2021-08-29 16:04:20 +0200

    add bootstrap

--- unnamed-frontend/src/App.tsx
+++ unnamed-frontend/src/App.tsx
@@ -1,6 +1,12 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+
+
+
+// https://react-bootstrap.github.io/getting-started/introduction
 
 function App() {
   return (
