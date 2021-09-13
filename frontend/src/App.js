1d8af473df5c589f8aa59ce41930cf393f281037
blc <blc@elcoyote.dk>
2021-09-13 21:34:00 +0200

    refactor

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,5 @@
-import React from 'react';
 import logo from './logo.svg';
 import './App.css';
-import 'bootstrap/dist/css/bootstrap.min.css';
-
-
-
-
-// https://react-bootstrap.github.io/getting-started/introduction
 
 function App() {
   return (
@@ -14,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.tsx</code> and save to reload.
+          Edit <code>src/App.js</code> and save to reload.
         </p>
         <a
           className="App-link"
