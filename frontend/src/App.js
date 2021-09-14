c99cb87ca9e86c9f0504dc5e518fa5ad077cf53e
Brian Leach Christensen <blc@nuuday.dk>
2021-09-14 11:08:33 +0200

    Update App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,14 @@
 import logo from './logo.svg';
 import './App.css';
 import WeatherContainer from './Components/WeatherContainer';
-import settings from './Settings/config'
+import settings from './Settings/config';
+import Container from "react-bootstrap/Container";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
 
 function App() {
 
-
+  // 
   return (
     <div className="App">
       <header className="App-header">
@@ -23,9 +26,19 @@ function App() {
         </a>
       </header>
       <WeatherContainer host={settings.apiHost}></WeatherContainer>
+      <Container>
+        <Row className="justify-content-md-center">
+          <Col xs lg="">
+            1 of 3
+          </Col>
+          <Col md="auto">Variable width content</Col>
+          <Col xs lg="2">
+            3 of 3
+          </Col>
+        </Row>
+      </Container>
     </div>
   );
-
 }
 
 
