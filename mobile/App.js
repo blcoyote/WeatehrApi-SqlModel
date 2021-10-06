7edb6c82825f2e49d12a835207dbbe3bfe3e3410
Brian Leach Christensen <blc@tdc.dk>
2021-10-06 14:46:36 +0200

    partial migration to expo

--- mobile/App.js
+++ mobile/App.js
@@ -1,11 +1,16 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import React from "react";
+import settings from "./Settings/config";
+import { StyleSheet, Text, View } from "react-native";
+import "bootstrap/dist/css/bootstrap.css";
+import WeatherContainer from "./Components/WeatherContainer";
 
 export default function App() {
+  document.title = "Vejret i Galten";
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <WeatherContainer host={settings.apiHost}></WeatherContainer>
       <StatusBar style="auto" />
     </View>
   );
@@ -14,8 +19,8 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
