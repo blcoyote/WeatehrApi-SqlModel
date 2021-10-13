73e42fea8ae3fb2142485af59218c972f702776a
Brian Leach Christensen <blc@nuuday.dk>
2021-10-13 14:51:25 +0200

    Update da_DK.js

--- expofrontend/Localization/da_DK.js
+++ expofrontend/Localization/da_DK.js
@@ -1,4 +1,9 @@
 export default locale = {
-  ui: {},
-  weather: {},
+  ui: {}, //translate ui elements
+  weather: {}, // translate weather observation lables
+  graphs: {}, // translate graph lables
+  info: {
+    privacy: {}, // Translate app/developer information
+    info: {}, // translate privacy policy
+  },
 };
